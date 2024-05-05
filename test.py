ed5f5bfbe38abca730dcebdf96ba2b5acd9e0eb4
aryapg <110161253+aryapg@users.noreply.github.com>
2024-05-05 17:34:22 +0530

    test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ if uploaded_file is not None:
     for column in categorical_columns:
         X[column] = label_encoder.fit_transform(X[column])
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
     rf_model.fit(X_train, y_train)
 
